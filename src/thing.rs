813b12c56a789abb7457a7921c0b3efc1a1ad648
Catchawink <threeeasywords@gmail.com>
2024-04-13 03:48:06 -0400

    Update thing.rs

--- src/thing.rs
+++ src/thing.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 use bevy::prelude::*;
 
-#[derive(Serialize, Deserialize, Reflect, Clone, PartialEq, ::prost::Message)]
+#[derive(Serialize, Deserialize, Reflect, Clone, PartialEq, ::prost::Message, Hash, Eq)]
 pub struct Thing {
     #[prost(string, tag = "1")]
     pub id: ::prost::alloc::string::String
