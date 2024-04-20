3d274baa1fb823d25758674f59e016f94e30cc01
Catchawink <threeeasywords@gmail.com>
2024-04-20 13:15:25 -0400

    Updated files.

--- src/thing.rs
+++ src/thing.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use serde::{Deserialize, Serialize};
 use bevy::prelude::*;
 
@@ -5,4 +7,10 @@ use bevy::prelude::*;
 pub struct Thing {
     #[prost(string, tag = "1")]
     pub id: ::prost::alloc::string::String
+}
+
+impl Display for Thing {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.id.fmt(f)
+    }
 }
