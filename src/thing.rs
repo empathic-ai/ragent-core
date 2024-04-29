19d568ac88498febe09428efd025b0c708ceeb5c
Catchawink <threeeasywords@gmail.com>
2024-04-28 20:38:20 -0400

    Update thing.rs

--- src/thing.rs
+++ src/thing.rs
@@ -1,12 +1,42 @@
 use std::fmt::Display;
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use bevy::{prelude::*, utils::Uuid};
 
-use serde::{Deserialize, Serialize};
-use bevy::prelude::*;
-
-#[derive(Serialize, Deserialize, Reflect, Clone, PartialEq, ::prost::Message, Hash, Eq)]
+#[derive(Reflect, Clone, PartialEq, ::prost::Message, Hash, Eq)]
 pub struct Thing {
     #[prost(string, tag = "1")]
     pub id: ::prost::alloc::string::String
+}
+
+impl Thing {
+    pub fn new() -> Self {
+        return Self::from(Uuid::new_v4().to_string().as_str())
+    }
+
+    pub fn from(text: &str) -> Self {
+        return Self {
+            id: text.replace("-", "")
+        }
+    }
+}
+
+impl Serialize for Thing {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(&self.id)
+    }
+}
+
+impl<'de> Deserialize<'de> for Thing {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let id = String::deserialize(deserializer)?;
+        Ok(Thing { id })
+    }
 }
 
 impl Display for Thing {
