d23ae43ed0395ff6c4ce51a4fe8b25c4fb49decd
Catchawink <threeeasywords@gmail.com>
2024-04-30 22:34:46 -0400

    Update task.rs

--- src/task.rs
+++ src/task.rs
@@ -1,6 +1,13 @@
+#[cfg(feature = "bevy")]
 use bevy::{prelude::*, reflect::Typed};
 use documented::Documented;
 
+#[cfg(feature = "bevy")]
 /// A trait implemented by all tasks
 pub trait Task: Reflect + Typed + Documented + FromReflect + Clone {
+}
+
+#[cfg(not(feature = "bevy"))]
+/// A trait implemented by all tasks
+pub trait Task: Documented + Clone {
 }
