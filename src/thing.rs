df7874cc242d6e1842a76388409d7e447f0b2b5b
Catchawink <threeeasywords@gmail.com>
2024-05-21 15:34:20 -0400

    Update thing.rs

--- src/thing.rs
+++ src/thing.rs
@@ -1,50 +1,7 @@
-use std::fmt::Display;
+use std::{fmt::Display, marker::PhantomData};
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use uuid::Uuid;
 
 #[cfg(feature = "bevy")]
 use bevy::{prelude::*};
 
-#[derive(Clone, PartialEq, ::prost::Message, Hash, Eq)]
-#[cfg_attr(feature = "bevy", derive(Reflect))]
-pub struct Thing {
-    #[prost(string, tag = "1")]
-    pub id: ::prost::alloc::string::String
-}
-
-impl Thing {
-    pub fn new() -> Self {
-        return Self::from(Uuid::new_v4().to_string().as_str())
-    }
-
-    pub fn from(text: &str) -> Self {
-        return Self {
-            id: text.replace("-", "")
-        }
-    }
-}
-
-impl Serialize for Thing {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        serializer.serialize_str(&self.id)
-    }
-}
-
-impl<'de> Deserialize<'de> for Thing {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let id = String::deserialize(deserializer)?;
-        Ok(Thing { id })
-    }
-}
-
-impl Display for Thing {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        self.id.fmt(f)
-    }
-}
